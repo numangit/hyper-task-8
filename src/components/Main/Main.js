e489e667b4a44da7cd80bf1b1bd6b90b455b3862
numangit <abdulnumanjob@gmail.com>
2022-09-29 14:44:39 +0600

    minor mistakes corrected

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => {
     return (
         <div className="col col-md-9 order-last order-md-first  bg-navy">
             <span className="my-4 d-flex justify-content-center ms-4"><img className="w-10 me-4" src={logo} alt="" /><h2 className="fw-bolder fs-1 ">Hyper Task</h2></span>
-            <h4 className="ms-lg-5 text-center text-lg-start mt-4">Select today’s exercise</h4>
+            <h4 className="ms-lg-5 text-center text-lg-start mt-4">Select today’s activities</h4>
             <Activities activities={activities} addToList={addToList}></Activities>
             <hr className="mt-lg-5" />
             <Faq></Faq>
